29397ba7036eca7cd3ceb9f11129fb75302135b0
pajamaw <pjpeterww@gmail.com>
2016-03-18 11:57:59 -0400

    Done.

--- recipe.js
+++ recipe.js
@@ -1,1 +1,23 @@
 'use strict';
+
+function addIngredient(recipe, ingredient, quantity){
+  recipe[ingredient] = quantity;
+  return recipe;
+}
+
+function removeIngredient(recipe, ingredient){
+  delete recipe[ingredient];
+  return recipe;
+}
+
+function updateIngredient(recipe, ingredient, quantity){
+  recipe[ingredient] = quantity;
+  return recipe;
+}
+
+function readRecipe(recipe){
+  for (var key in recipe) {
+    console.log("this recipe calls for " + recipe[ingredient]+ " of "+ key);
+  }
+} 
+
